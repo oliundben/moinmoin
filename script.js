cf94be3080d2add8e49bb7816f446973f13cb232
BenBals <benjustusbals@gmail.com>
2016-05-03 15:04:08 +0200

    add ga code

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 var shiftWindow = function() { scrollBy(0, -70) };
 if (location.hash) shiftWindow();
 window.addEventListener("hashchange", shiftWindow);
+
+(function () {
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-77255637-1', 'auto');
+    ga('send', 'pageview');
+})()
