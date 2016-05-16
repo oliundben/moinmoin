146ebbfbc1d121e7f02449b783a909d5f61a5935
BenBals <benjustusbals@gmail.com>
2016-05-16 17:32:58 +0200

    basic fliper

--- script.js
+++ script.js
@@ -3,11 +3,34 @@ if (location.hash) shiftWindow();
 window.addEventListener("hashchange", shiftWindow);
 
 (function () {
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
     ga('create', 'UA-77255637-1', 'auto');
     ga('send', 'pageview');
 })()
+
+// doing the image carousel stuff
+var facts = document.querySelector('#facts').children
+var current = 0
+var init = function () {
+    show()
+    var timer = setInterval(next, 1000)
+}
+
+var show = function () {
+    for (var i = 0; i < facts.length; i++) {
+        facts[i].classList.add('invisible')
+    }
+    facts[current].classList.remove('invisible')
+}
+
+var next = function () {
+    current = (current + 1) % facts.length
+    console.log('current')
+    show(current)
+}
+
+init()
